bfc018756c8c315f47cf626704331524751d981e
Jpdl925 <jtpg925@gmail.com>
2024-04-24 19:18:17 -0700

    Complete Project

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,23 @@
-alert("test");
+// Button used for fetching, and changing the text on screen to the randomly generated dad joke
+let jokeBtn = document.getElementById("jokeBtn").addEventListener('click', () =>{
+    dadJoke();
+});
+// Text that is changed from the button click to a randomly generated dad joke from the api
+let jokeTxt = document.getElementById("jokeTxt");
+// Laugh track that plays once button is pressed
+let audio = new Audio("/media/laughTrack.mp3");
+
+
+async function dadJoke(){
+    let apiResponse = await fetch("https://icanhazdadjoke.com", {
+        headers: {
+            'accept': 'application/json',
+        }
+    }).then((Response) => Response.json());;
+    // Setting the jokeTxt to the randomly generated dad joke once button is clicked
+    jokeTxt.innerText = apiResponse.joke;
+    // Playing laugh track audio upon click
+    audio.play();
+}
+
+
